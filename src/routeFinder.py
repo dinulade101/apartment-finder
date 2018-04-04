3fac54425f90f61c24a9578a67fb2203d9a598ee
Dinula De Silva <dinulasd@gmail.com>
2018-04-04 08:13:17 -0600

    commented code and made things a little nicer

--- src/routeFinder.py
+++ src/routeFinder.py
@@ -2,6 +2,7 @@ from graph import Graph
 from breadth_first_search import *
 from heapviz import *
 import math
+import settings
 
 class RouteFinder:
     """
@@ -21,9 +22,12 @@ class RouteFinder:
         to convert to meters. 
         However, this will allow for comparison of distances of apartments within the constraints of this program.
         '''
+
+        #compute distance between each vertex in a path array to get the total distance
         distanceSoFar = 0
         for i, val in enumerate(path):
             if i == (len(path)-1):
+                #skip the first vertex beacuse there is no vertex before it 
                 pass
             else:
                 distanceSoFar += convertLatLonDistToMetres(self.locations[val], self.locations[path[i+1]])
@@ -32,39 +36,44 @@ class RouteFinder:
         return distanceSoFar
 
     def computePathToUni(self, houseLocationInCoords):
+        #find the corresponding vertex using the lat and lon coordinates of the house, as well as the university 
         houseVertex = computeClosestVertexFromLatLonCoord(houseLocationInCoords, self.locations)
         uniVertex = computeClosestVertexFromLatLonCoord((5352133.1248, -11352133.1248), self.locations)
-        #print("vertices", houseVertex, uniVertex)
 
+        #calls function to build least cost path from each LRT station to house under consideration
         self.computePathFromLRTToHouse(houseVertex, "listOfLRTStations.txt")
 
+        #calls function to find least cost path from the house vertex to the vertex of the University of Alberta
         path = self.least_cost_path(houseVertex, uniVertex)
+
+        #compute distance in km from path 
         dist = self.computeDistanceFromPath(path)
 
+        #return both the path and distance
         return path, dist
     
     def computePathFromLRTToHouse(self, house, lrtfilepath):
         dictOfLRT = dict()
         dictOfPaths = dict()
+
+        #get LRT coordinates from text file and add to dictionary
         with open(lrtfilepath, 'r') as myFile:
             for line in myFile:
                 lineItems = line.split(',')
-                #print(float(lineItems[1]))
                 coord = (float(lineItems[1])*100000, float(lineItems[2])*100000)
-                #print(lineItems[0], len(lineItems[0]))
                 dictOfLRT[(lineItems[0])]=computeClosestVertexFromLatLonCoord(coord, self.locations)
         
-        #print(dictOfLRT)
+        #iterate through dictionary to find least cost path from each LRT to house 
         for key, val in dictOfLRT.items():
             dictOfPaths[key] = self.least_cost_path(val, house)
         
-        #for key, val in dictOfPaths.items():
-            #print(self.computeDistanceFromPath(val))
 
         
     def least_cost_path(self, start, end):
         reached = {}
         events = BinaryHeap()
+
+        #insert start locatin with time of 0 into the heap 
         events.insert((start, start, 0), 0)
         while (len(events)) > 0:
             pair, time = events.popmin()
@@ -72,8 +81,12 @@ class RouteFinder:
             #account for heuristic so it is not compounded
             if (pair[2]):
                 time -= pair[2]
+
             if pair[1] not in reached:
+                # add to reached dictionary 
                 reached[pair[1]] = pair[0]
+
+                #consider neighbors around each point and distance to each neighbor
                 for neighbour in self.graph.neighbours(pair[1]):
                     point1 = self.locations[pair[1]]
                     point2 = self.locations[neighbour]
@@ -81,22 +94,22 @@ class RouteFinder:
                     # maybe we could save distances between objects in a dict, and reuse them?
                     
 
-                    #add heuristic in for the form of euclidean distance
+                    # add heuristic in for the form of euclidean distance. This will helps lead the searching algorithm
+                    # more towards the end location 
+
                     heuristic = euclidean_distance(point2, self.locations[end])
-                    #print(manhattan_distance(point1, point2))
+                    
+                    # add key and val to BinaryHeap called events 
                     events.insert((pair[1], neighbour, heuristic), time + euclidean_distance(point1, point2) + heuristic)
+            
             # end loop right after destination is popped
             if (pair[1] == end):
-                #print(pair[1], end)
                 break
             
+        # use the get_path included with the breadth_first_search helper file to find path from reached dictionary
         return get_path(reached, start, end)
 
-
-
-'''def manhattan_distance(point1, point2):
-   return (((point1[0]-point2[1])**2+(point1[1]-point2[0])**2) ** 0.5)
-'''
+#use formula for convert difference in lat and lon coordinates between points into km
 def convertLatLonDistToMetres(point1, point2):
     diffX = abs(point2[0]-point1[0])/100000
     diffY = abs(point2[1]-point1[1])/100000
@@ -119,25 +132,26 @@ def computeClosestVertexFromLatLonCoord(locationInCoords, locations):
     # initialize variables to keep track closest vertex as we interate through the city graph
     closestDist = float('inf')
     locationVertex = 0
+
+    # loop through all items in locations dictionary until we find the vertex that is closest to the given lat and lon coordinate
     for key, val in locations.items():
-        #print(val)
         dist = euclidean_distance(locationInCoords, val)
         if dist < closestDist:
-            #print(locationVertex)
             locationVertex = key 
             closestDist = dist
-        #print(manhattanDist)
     return locationVertex
     
 
 def load_city_graph(filename):
-    "loads up the graph of a given city (only Edmonton for now) from a given textfile"
+    #loads up the graph of a given city (only Edmonton for now) from a given textfile
+
 
     vertices = []
     edges = []
     locations = dict()
     graph = Graph()
 
+    #parse txt file containing vertices and indices to build graph 
     with open(filename, 'r') as myfile:
         for line in myfile:
             lineItems = line.split(',')
@@ -147,10 +161,10 @@ def load_city_graph(filename):
             elif (lineItems[0] == 'E'):
                 graph.add_edge((int(lineItems[1]), int(lineItems[2])))
         
-    #print(len(locations))
     return graph, locations
 
 
+# some testing code to ensure everything works 
 if __name__ == "__main__":
     #print(manhattan_distance([0,0], [3,4]))
     new = RouteFinder("edmonton.txt")
---
+++ src/settings.py
@@ -0,0 +1,5 @@
+'''
+This file contains all the settings such as the University coordinates for the program 
+'''
+
+
